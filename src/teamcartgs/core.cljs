78f1e4332eed0954554cd93a895f22b1f949ddcb
Eric Cheek <cheek@fastmail.com>
2016-10-12 23:44:23 -0700

    Basic workflow support

--- src/teamcartgs/core.cljs
+++ src/teamcartgs/core.cljs
@@ -2,18 +2,23 @@
   (:require [teamcartgs.entry-points]))
 
 (def code-version "1.0.0")
+(def product-lookup-url "https://lambda.teamcart.io/items-details")
+(def checkout-url "https://teamcart.io/checkout#")
+
+;; assumes range is 1d either columns or vectors
+(defn range-to-vec [range]
+  (->
+   range
+   js->clj
+   flatten))
+
 
 (defn fetch-products-from-api [product-urls]
   (let
-      [url "https://lambda.teamcart.io/items-details"
-       
+      [url product-lookup-url
        payload (->>
                 {:product-urls (distinct product-urls)
-                 ;; (map
-                 ;;  #(js/encodeURIComponent %)
-                 ;;  product-urls)
-                 :version code-version
-                 }
+                 :version code-version}
                 clj->js
                 (.stringify js/JSON))
        options {:method "post"
@@ -38,19 +43,12 @@
     "Arguments must be ranges"
     :default
     (let
-        [url-vec (->
-                  url-range
-                  js->clj
-                  flatten)
-
-         key-vec (->
-                  key-range
-                  js->clj
-                  flatten)
+        [url-vec (range-to-vec url-range)
+         key-vec (range-to-vec key-range)
 
          results-map (->
                       url-vec
-                      fetch-products-from-api 
+                      fetch-products-from-api
                       (aget "result")
                       js->clj)
 
@@ -63,7 +61,7 @@
                 (js/Date.))
                (get item %))
             key-vec))
-         
+
          output-grid
          (->>
           url-vec
@@ -73,9 +71,46 @@
       output-grid)))
 
 
-(defn ^:export get-checkout-link [product-range quantity-range]
-  )
+(defn ^:export get-checkout-link [product-id-range quantity-range]
+  (cond
+    (or
+     (not (array? product-id-range))
+     (not (array? quantity-range)))
+    "Arguments must be ranges"
 
-(defn ^:export teamcart-test [url-range key-range]
-  ;;(test-generate-grid url-range key-range)
-  )
+    (not=
+     (.-length product-id-range)
+     (.-length quantity-range))
+    "Argument rangges must match in size"
+
+    :default
+    (let
+        [product-vec (range-to-vec product-id-range)
+         qty-vec (range-to-vec quantity-range)
+
+         valid-items (->>
+                      (zipmap product-vec qty-vec)
+                      (filter (fn [[product qty]]
+                                   (and
+                                    (string? product)
+                                    (number? qty)
+                                    (pos? qty))))
+                      (into {}))
+         ;; TODO: assert all items have same locale
+         locale (some->
+                 valid-items
+                 first
+                 first
+                 (.substring 0 2))
+
+         link-url (.join
+                   (->>
+                    valid-items
+                    (mapcat (fn [[product qty]]
+                              [(.substring product 2) qty]))
+                    (into [(str checkout-url locale)])
+                    into-array)
+                   ",")]
+      (if (empty? valid-items)
+        "Cart is empty"
+        link-url))))
